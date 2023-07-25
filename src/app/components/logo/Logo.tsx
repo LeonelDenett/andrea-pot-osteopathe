60b48a74f680cad0fd31a3241f9625d30377876b
LeonelDenett <leoneldenett.dev@hotmail.com>
2023-07-25 17:55:54 +0200

    height logo fixed

--- src/app/components/logo/Logo.tsx
+++ src/app/components/logo/Logo.tsx
@@ -1,11 +1,7 @@
 // Next
 import Image from 'next/image';
 // Styles
-<<<<<<< HEAD
 import styles from './Logo.module.css';
-=======
-import styles from './logo.module.css';
->>>>>>> 104d3f4807371714289978b18c92f7a7f72745a4
 // Mui Components
 import Box from '@mui/material/Box';
 // Image
